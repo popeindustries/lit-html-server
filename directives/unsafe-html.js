58d5ae345dcda6e4f5e7b753c579418f0fce5d42
Alexander Pope <alex@pope-industries.com>
2018-09-13 14:36:34 +0200

    throw on unsafe-html directive type error

--- directives/unsafe-html.js
+++ directives/unsafe-html.js
@@ -10,6 +10,9 @@ const { NO_ESCAPE } = require('../lib/string.js');
  */
 module.exports = function unsafeHTML(value) {
   return directive((part) => {
+    if (typeof value !== 'string') {
+      throw Error('The `unsafe-html` directive must be used on a string');
+    }
     part.setValue(`${NO_ESCAPE}${value}`);
   });
 };
