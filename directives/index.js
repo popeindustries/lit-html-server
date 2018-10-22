dcee34616c37c69e022763b0027d6e0a87ef587f
Alexander Pope <alex@pope-industries.com>
2018-10-22 18:14:57 +0200

    expose directives as package

---
+++ directives/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  classMap: require('./classMap.js'),
+  guard: require('./guard.js'),
+  ifDefined: require('./if-defined.js'),
+  repeat: require('./repeat.js'),
+  styleMap: require('./styleMap.js'),
+  unsafeHtml: require('./unsafe-html.js'),
+  until: require('./until.js'),
+  when: require('./when.js')
+};
