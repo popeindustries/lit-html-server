39501f48e1bfde46c917a634255ad591506a1c83
Alexander Pope <alex@pope-industries.com>
2018-11-29 19:16:14 +0100

    begin until directive refactor

--- directives/until.js
+++ directives/until.js
@@ -4,14 +4,13 @@ const { directive } = require('../lib/directive.js');
 
 module.exports = {
   /**
-   * Render 'defaultContent'.
+   * Renders one of a series of values, including Promises, in priority order.
    * Not possible to render more than once in a server context,
    * so this is a no-op.
-   * @param {Promise<*>} promise
-   * @param {*} defaultContent
+   * @param {...*} args
    * @returns {function}
    */
-  until: directive((promise, defaultContent) => (part) => {
-    part.setValue(defaultContent);
+  until: directive((...args) => (part) => {
+    part.setValue(args[0]);
   })
 };
