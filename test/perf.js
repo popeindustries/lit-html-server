8e8a879071125520463a2108701ec01a1efc8df6
Alexander Pope <alex@pope-industries.com>
2019-01-24 15:03:14 +0100

    update perf test

---
+++ test/perf.js
@@ -0,0 +1,40 @@
+const autocannon = require('autocannon');
+const { fork } = require('child_process');
+const path = require('path');
+
+const args = process.argv.slice(2);
+const async = args.includes('async');
+const buffer = args.includes('buffer');
+const url = `http://localhost:3000?${async ? 'async' : ''}${
+  buffer ? `${async ? '&' : ''}buffer` : ''
+}`;
+
+const child = fork(path.resolve(__dirname, './server.js'), { silent: true });
+
+(async () => {
+  await stress();
+  child.kill();
+  process.exit();
+})();
+
+function stress() {
+  console.log(`Stress testing with async:${async} and buffer:${buffer}\n`);
+  return new Promise((resolve, reject) => {
+    const instance = autocannon(
+      {
+        url,
+        connections: 100,
+        pipelining: 10,
+        duration: 5
+      },
+      async (err, results) => {
+        // console.log(results);
+        if (err) {
+          return reject(err);
+        }
+        resolve(results);
+      }
+    );
+    autocannon.track(instance, { renderProgressBar: true });
+  });
+}
--- test/server.js
+++ test/server.js
@@ -1,13 +1,25 @@
 'use strict';
 
-const { html, renderToStream } = require('../index.js');
+const { html, renderToStream, renderToString } = require('../index.js');
 const http = require('http');
+const lorem = require('lorem-ipsum');
 
 http
   .createServer((req, res) => {
-    const d = new Date();
+    const data = {
+      async: /async/.test(req.url),
+      title: new Date().toISOString(),
+      isTrue: Math.random() > 0.5,
+      number: Math.random() * 100
+    };
     res.writeHead(200);
-    renderToStream(template({ title: d.toISOString(), isTrue: Math.random() > 0.5 })).pipe(res);
+    if (/buffer/.test(req.url)) {
+      renderToString(template(data)).then((html) => {
+        res.end(html);
+      });
+    } else {
+      renderToStream(template(data)).pipe(res);
+    }
   })
   .listen(3000);
 
@@ -24,50 +36,43 @@ function template(data) {
       <body>
         <header><h1 ?negative="${data.isTrue}">${data.title}</h1></header>
         <main>
+          <ol>
+            ${Array.from({ length: Math.random() * 50 }).map(
+              (v, i) => html`
+                <li>
+                  ${i} - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie
+                  lacus eget ipsum pellentesque, quis ullamcorper enim semper
+                </li>
+              `
+            )}
+          </ol>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie lacus eget ipsum
-            pellentesque, quis ullamcorper enim semper. Curabitur non dignissim felis. Donec rhoncus
-            faucibus ultrices. Sed ut orci ut leo luctus vestibulum. In iaculis dolor sed ante
-            pulvinar consequat. In nec metus tempor, fermentum tellus nec, pulvinar enim. Ut ex dui,
-            pellentesque eget ipsum ut, elementum iaculis purus. Sed sed egestas mi.
+            Duis eleifend nec lectus a ${data.number}. Suspendisse placerat mollis porta.
+            Pellentesque nec quam non sapien facilisis ultricies quis nec risus. Quisque feugiat dui
+            quis lectus iaculis, molestie pretium augue tincidunt. Suspendisse potenti. Pellentesque
+            habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            Suspendisse quis libero sagittis, vulputate magna in, laoreet sem. Ut elementum nunc
+            eget libero hendrerit eleifend at eget sem. Sed vel urna consequat, interdum massa in,
+            mollis justo. Pellentesque porttitor auctor sapien, sit amet elementum turpis imperdiet
+            ac. Cras luctus, sem vel finibus vehicula, mauris tellus iaculis orci, sit amet sodales
+            velit augue non felis. Nullam vehicula gravida justo non lacinia.
           </p>
           <p>
-            Duis eleifend nec lectus a malesuada. Suspendisse placerat mollis porta. Pellentesque
-            nec quam non sapien facilisis ultricies quis nec risus. Quisque feugiat dui quis lectus
-            iaculis, molestie pretium augue tincidunt. Suspendisse potenti. Pellentesque habitant
-            morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse quis
-            libero sagittis, vulputate magna in, laoreet sem. Ut elementum nunc eget libero
-            hendrerit eleifend at eget sem. Sed vel urna consequat, interdum massa in, mollis justo.
-            Pellentesque porttitor auctor sapien, sit amet elementum turpis imperdiet ac. Cras
-            luctus, sem vel finibus vehicula, mauris tellus iaculis orci, sit amet sodales velit
-            augue non felis. Nullam vehicula gravida justo non lacinia.
-          </p>
-          <p>
-            ${wait(`Nam vel libero metus. Fusce euismod turpis nec ligula finibus, vel placerat felis consequat. Nam nisi erat,
-        tincidunt et fringilla vel, volutpat sed nisi. Integer non eleifend purus. Cras eget urna cursus, ullamcorper elit
-        ac, iaculis odio. Quisque et leo ac ante pellentesque semper. Nulla interdum lacus nunc, et facilisis nunc vestibulum
-        quis. Vestibulum suscipit molestie fermentum. Nulla quis semper nibh. Nam accumsan cursus lectus ac imperdiet.`)}
+            ${nestedTemplate(data)}
           </p>
           <p>
             Mauris lobortis, nisl vitae hendrerit vulputate, est lacus efficitur ipsum, nec blandit
-            nisi diam in dolor. Proin sollicitudin laoreet nisi a vulputate. Praesent non congue
-            quam, ut sodales risus. Curabitur ornare elit at suscipit pulvinar. Suspendisse vitae
-            orci a justo laoreet vestibulum quis et ex. Integer nec risus aliquam, convallis erat
-            in, dignissim sapien. Suspendisse metus felis, volutpat a tempus ut, semper in tortor.
-            Mauris rutrum dui in elit blandit, non vestibulum felis cursus. Sed mollis consectetur
-            eros a lobortis. Sed lobortis lorem eu metus auctor tempus. Sed faucibus sit amet urna
-            vel accumsan. Cras luctus in lorem ac tempor. Sed ullamcorper consectetur ligula sed
-            malesuada.
+            nisi diam in dolor. Proin ${JSON.stringify(data)} laoreet nisi a vulputate. Praesent non
+            congue quam, ut sodales risus. Curabitur ornare elit at suscipit pulvinar. Suspendisse
+            vitae orci a justo laoreet vestibulum quis et ex. Integer nec risus aliquam, convallis
+            erat in, dignissim sapien. Suspendisse metus felis, volutpat a tempus ut, semper in
+            tortor. Mauris rutrum dui in elit blandit, non vestibulum felis cursus. Sed mollis
+            consectetur eros a lobortis. Sed lobortis lorem eu metus auctor tempus. Sed faucibus sit
+            amet urna vel accumsan. Cras luctus in lorem ac tempor. Sed ullamcorper consectetur
+            ligula sed malesuada.
           </p>
           <p .prop="${data.title}" ?visible="${data.isTrue}">
-            ${wait(`Fusce hendrerit massa venenatis elit accumsan, id varius elit
-          varius. Fusce semper nisl non ligula faucibus hendrerit. Vestibulum et tellus non metus maximus viverra. Aenean scelerisque
-          lacus urna, tristique varius tortor interdum vel. Ut ac euismod mauris, et pretium dolor. Phasellus mattis, libero
-          quis fermentum tincidunt, massa orci tempor ante, eget tincidunt nunc libero ac eros. Fusce eget imperdiet purus. Sed
-          sem odio, consequat at tellus at, mattis gravida risus. In eu arcu sed sapien condimentum iaculis in in neque. Duis
-          condimentum ante lorem, quis elementum est aliquam ut. Ut imperdiet ex ac urna posuere, quis interdum nisl lacinia.
-          Pellentesque lacus lacus, sodales sed dolor in, imperdiet vulputate dui. Aenean sed leo eu augue semper semper. Donec
-          vulputate massa magna, semper lobortis purus luctus ac.`)}
+            ${nestedTemplate(data)}
           </p>
         </main>
       </body>
@@ -75,10 +80,20 @@ function template(data) {
   `;
 }
 
-function wait(content) {
+function nestedTemplate(data) {
+  const t = html`
+    ${lorem({ count: 2, units: 'paragraphs' })}
+  `;
+  if (data.async) {
+    return wait(t);
+  }
+  return t;
+}
+
+function wait(template) {
   new Promise((resolve) => {
     setTimeout(() => {
-      resolve(content);
-    }, Math.random() * 100);
+      resolve(template);
+    }, 50);
   });
 }
