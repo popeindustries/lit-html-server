7440384f8372f43f16632c1c6cc448eed3f12caf
Alexander Pope <alex@pope-industries.com>
2019-10-17 20:54:58 +0200

    update types

--- index.d.ts
+++ index.d.ts
@@ -1,224 +1,232 @@
-declare module '@popeindustries/lit-html-server' {
-  interface TemplateProcessor {
-    handleAttributeExpressions: (name: string, strings: TemplateStringsArray) => AttributePart;
-    handleTextExpression: () => NodePart;
-  }
+interface TemplateProcessor {
+  handleAttributeExpressions: (name: string, strings: TemplateStringsArray) => AttributePart;
+  handleTextExpression: () => NodePart;
+}
 
-  interface TemplateResultRenderer {
-    push: (chunk: Buffer) => boolean;
-    destroy: (err: Error) => void;
-  }
+interface TemplateResultRenderer {
+  push: (chunk: Buffer) => boolean;
+  destroy: (err: Error) => void;
+}
 
-  export const defaultTemplateProcessor: DefaultTemplateProcessor;
-  export const defaultTemplateResultProcessor: DefaultTemplateResultProcessor;
+type RenderOptions = {
+  serializePropertyAttributes: boolean;
+};
+
+export const defaultTemplateProcessor: DefaultTemplateProcessor;
+export const defaultTemplateResultProcessor: DefaultTemplateResultProcessor;
+
+/**
+ * Define new directive for "fn".
+ * The passed function should be a factory function,
+ * and must return a function that will eventually be called with a Part instance
+ */
+export function directive<F extends (...args: Array<any>) => object>(f: F): F;
+
+/**
+ * Determine if "part" is an AttributePart
+ */
+export function isAttributePart(part: Part): boolean;
+
+/**
+ * Determine if "part" is a PropertyAttributePart
+ */
+export function isPropertyAttributePart(part: Part): boolean;
+
+/**
+ * Determine if "part" is a NodePart
+ */
+export function isNodePart(part: Part): boolean;
+
+/**
+ * Determine whether "result" is a TemplateResult
+ */
+export function isTemplateResult(result: TemplateResult): boolean;
+
+/**
+ * A value for strings that signals a Part to clear its content
+ */
+export const nothingString: string;
+export const templateCache: Map<TemplateStringsArray, Template>;
+
+/**
+ * A prefix value for strings that should not be escaped
+ */
+export const unsafePrefixString: string;
+
+/**
+ * Interprets a template literal as an HTML template that can be
+ * rendered as a Readable stream or String
+ */
+export function html(strings: TemplateStringsArray, ...values: Array<unknown>): TemplateResult;
+export function svg(strings: TemplateStringsArray, ...values: Array<unknown>): TemplateResult;
+
+/**
+ * Render a template result to a string resolving Promise.
+ */
+export function renderToString(result: TemplateResult, options?: RenderOptions): Promise<string>;
+
+/**
+ * Render a template result to a Readable stream
+ */
+export function renderToStream(
+  result: TemplateResult,
+  options?: RenderOptions
+): import('stream').Readable;
+
+/**
+ * Render a template result to a Buffer resolving Promise.
+ */
+export function renderToBuffer(result: TemplateResult, options?: RenderOptions): Promise<Buffer>;
+
+/**
+ * Base class interface for Node/Attribute parts
+ */
+export class Part {
+  /**
+   * Store the current value.
+   * Used by directives to temporarily transfer value
+   * (value will be deleted after reading).
+   */
+  setValue(value: unknown): void;
+}
+
+/**
+ * A dynamic template part for text nodes
+ */
+export class NodePart extends Part {
+  /**
+   * Retrieve resolved value given passed "value"
+   */
+  getValue(value: unknown, options?: RenderOptions): unknown;
+}
+
+/**
+ * A dynamic template part for attributes.
+ * Unlike text nodes, attributes may contain multiple strings and parts.
+ */
+export class AttributePart extends Part {
+  /*
+   * @param { Array<any> } values
+   * @returns { Buffer|Promise<Buffer> }
+   */
+  getValue(values: Array<unknown>, options?: RenderOptions): Buffer | Promise<Buffer>;
+}
+
+/**
+ * A dynamic template part for boolean attributes.
+ * Boolean attributes are prefixed with "?"
+ */
+export class BooleanAttributePart extends AttributePart {
+  /**
+   * Retrieve resolved string Buffer from passed "values".
+   */
+  getValue(values: Array<unknown>, options?: RenderOptions): Buffer | Promise<Buffer>;
+}
+
+/**
+ * A dynamic template part for property attributes.
+ * Property attributes are prefixed with "."
+ */
+export class PropertyAttributePart extends AttributePart {
+  /**
+   * Retrieve resolved string Buffer from passed "values".
+   * Properties have no server-side representation,
+   * so always returns an empty string.
+   */
+  getValue(values: Array<unknown>, options?: RenderOptions): Buffer;
+}
+
+/**
+ * A dynamic template part for event attributes.
+ * Event attributes are prefixed with "@"
+ */
+export class EventAttributePart extends AttributePart {
+  /**
+   * Retrieve resolved string Buffer from passed "values".
+   * Event bindings have no server-side representation,
+   * so always returns an empty string.
+   */
+  getValue(values: Array<unknown>, options?: RenderOptions): Buffer;
+}
+
+/**
+ * Class representing the default Template processor.
+ * Exposes factory functions for generating Part instances to use for
+ * resolving a template's dynamic values.
+ */
+export class DefaultTemplateProcessor {
+  /**
+   * Create part instance for dynamic attribute values
+   */
+  handleAttributeExpressions(name: string, strings?: Array<string>): AttributePart;
 
   /**
-   * Define new directive for "fn".
-   * The passed function should be a factory function,
-   * and must return a function that will eventually be called with a Part instance
+   * Create part instance for dynamic text values
    */
-  export function directive<F extends (...args: Array<any>) => object>(f: F): F;
+  handleTextExpression(): NodePart;
+}
+
+/**
+ * Class for the default TemplateResult processor
+ * used by Promise/Stream TemplateRenderers.
+ *
+ * @implements TemplateResultProcessor
+ */
+export class DefaultTemplateResultProcessor {
+  /**
+   * Process "stack" and push chunks to "renderer"
+   */
+  getProcessor(
+    renderer: TemplateResultRenderer,
+    stack: Array<unknown>,
+    highWaterMark?: number,
+    options?: RenderOptions
+  ): () => void;
 
   /**
-   * Determine if "part" is an AttributePart
+   * Permanently destroy all remaining TemplateResults in "stack".
+   * (Triggered on error)
    */
-  export function isAttributePart(part: Part): boolean;
+  destroy(stack: Array<unknown>): void;
+}
+
+/**
+ * A class for consuming the combined static and dynamic parts of a lit-html Template.
+ * TemplateResults
+ */
+class TemplateResult {
+  /**
+   * Constructor
+   */
+  constructor(template: Template, values: Array<unknown>);
 
   /**
-   * Determine if "part" is a NodePart
+   * Consume template result content.
    */
-  export function isNodePart(part: Part): boolean;
+  read(options?: RenderOptions): unknown;
 
   /**
-   * Determine whether "result" is a TemplateResult
+   * Consume template result content one chunk at a time.
    */
-  export function isTemplateResult(result: TemplateResult): boolean;
+  readChunk(options?: RenderOptions): unknown;
+}
+
+/**
+ * A cacheable Template that stores the "strings" and "parts" associated with a
+ * tagged template literal invoked with "html`...`".
+ */
+class Template {
+  /**
+   * Create Template instance
+   */
+  constructor(strings: Array<TemplateStringsArray>, processor: TemplateProcessor);
 
   /**
-   * A value for strings that signals a Part to clear its content
+   * Prepare the template's static strings,
+   * and create Part instances for the dynamic values,
+   * based on lit-html syntax.
    */
-  export const nothingString: string;
-  export const templateCache: Map<TemplateStringsArray, Template>;
-
-  /**
-   * A prefix value for strings that should not be escaped
-   */
-  export const unsafePrefixString: string;
-
-  /**
-   * Interprets a template literal as an HTML template that can be
-   * rendered as a Readable stream or String
-   */
-  export function html(strings: TemplateStringsArray, ...values: Array<unknown>): TemplateResult;
-  export function svg(strings: TemplateStringsArray, ...values: Array<unknown>): TemplateResult;
-
-  /**
-   * Render a template result to a string resolving Promise.
-   * *Note* that TemplateResults are single use, and can only be rendered once.
-   */
-  export function renderToString(result: TemplateResult): Promise<string>;
-
-  /**
-   * Render a template result to a Readable stream
-   * *Note* that TemplateResults are single use, and can only be rendered once.
-   */
-  export function renderToStream(result: TemplateResult): import('stream').Readable;
-
-  /**
-   * Render a template result to a Buffer resolving Promise.
-   * *Note* that TemplateResults are single use, and can only be rendered once.
-   */
-  export function renderToBuffer(result: TemplateResult): Promise<Buffer>;
-
-  /**
-   * Base class interface for Node/Attribute parts
-   */
-  export class Part {
-    /**
-     * Store the current value.
-     * Used by directives to temporarily transfer value
-     * (value will be deleted after reading).
-     */
-    setValue(value: unknown): void;
-  }
-
-  /**
-   * A dynamic template part for text nodes
-   */
-  export class NodePart extends Part {
-    /**
-     * Retrieve resolved value given passed "value"
-     */
-    getValue(value: unknown): unknown;
-  }
-
-  /**
-   * A dynamic template part for attributes.
-   * Unlike text nodes, attributes may contain multiple strings and parts.
-   */
-  export class AttributePart extends Part {
-    /*
-     * @param { Array<any> } values
-     * @returns { Buffer|Promise<Buffer> }
-     */
-    getValue(values: Array<unknown>): Buffer | Promise<Buffer>;
-  }
-
-  /**
-   * A dynamic template part for boolean attributes.
-   * Boolean attributes are prefixed with "?"
-   */
-  export class BooleanAttributePart extends AttributePart {
-    /**
-     * Retrieve resolved string Buffer from passed "values".
-     */
-    getValue(values: Array<unknown>): Buffer | Promise<Buffer>;
-  }
-
-  /**
-   * A dynamic template part for property attributes.
-   * Property attributes are prefixed with "."
-   */
-  export class PropertyAttributePart extends AttributePart {
-    /**
-     * Retrieve resolved string Buffer from passed "values".
-     * Properties have no server-side representation,
-     * so always returns an empty string.
-     */
-    getValue(values: Array<unknown>): Buffer;
-  }
-
-  /**
-   * A dynamic template part for event attributes.
-   * Event attributes are prefixed with "@"
-   */
-  export class EventAttributePart extends AttributePart {
-    /**
-     * Retrieve resolved string Buffer from passed "values".
-     * Event bindings have no server-side representation,
-     * so always returns an empty string.
-     */
-    getValue(values: Array<unknown>): Buffer;
-  }
-
-  /**
-   * Class representing the default Template processor.
-   * Exposes factory functions for generating Part instances to use for
-   * resolving a template's dynamic values.
-   */
-  export class DefaultTemplateProcessor {
-    /**
-     * Create part instance for dynamic attribute values
-     */
-    handleAttributeExpressions(name: string, strings?: Array<string>): AttributePart;
-
-    /**
-     * Create part instance for dynamic text values
-     */
-    handleTextExpression(): NodePart;
-  }
-
-  /**
-   * Class for the default TemplateResult processor
-   * used by Promise/Stream TemplateRenderers.
-   *
-   * @implements TemplateResultProcessor
-   */
-  export class DefaultTemplateResultProcessor {
-    /**
-     * Process "stack" and push chunks to "renderer"
-     */
-    getProcessor(
-      renderer: TemplateResultRenderer,
-      stack: Array<unknown>,
-      highWaterMark?: number
-    ): () => void;
-
-    /**
-     * Permanently destroy all remaining TemplateResults in "stack".
-     * (Triggered on error)
-     */
-    destroy(stack: Array<unknown>): void;
-  }
-
-  /**
-   * A class for consuming the combined static and dynamic parts of a lit-html Template.
-   * TemplateResults
-   */
-  class TemplateResult {
-    /**
-     * Constructor
-     */
-    constructor(template: Template, values: Array<unknown>);
-
-    /**
-     * Consume template result content.
-     */
-    read(deep: boolean): unknown;
-
-    /**
-     * Consume template result content one chunk at a time.
-     */
-    readChunk(): unknown;
-  }
-
-  /**
-   * A cacheable Template that stores the "strings" and "parts" associated with a
-   * tagged template literal invoked with "html`...`".
-   */
-  class Template {
-    /**
-     * Create Template instance
-     */
-    constructor(strings: Array<TemplateStringsArray>, processor: TemplateProcessor);
-
-    /**
-     * Prepare the template's static strings,
-     * and create Part instances for the dynamic values,
-     * based on lit-html syntax.
-     */
-    _prepare(strings: Array<TemplateStringsArray>, processor: TemplateProcessor): void;
-  }
+  _prepare(strings: Array<TemplateStringsArray>, processor: TemplateProcessor): void;
 }
 
 declare module '@popeindustries/lit-html-server/directives/async-append.js' {
